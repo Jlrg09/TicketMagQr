a308a246ba6fb00a52933eb9a7093da281805895
Jlrg09 <joseromerolg009@gmail.com>
2025-08-29 11:24:29 -0500

    Change user email and password for new user creation

--- createUser.py
+++ createUser.py
@@ -1,11 +1,11 @@
 from database import session_credenciales, Usuario
 import bcrypt
 
-password = "1207311309Fran"
+password = "admin"
 
 hashed_password = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
 
-nuevo_usuario = Usuario(email="joseromerolg@unimagdalena.edu.co", password=hashed_password)
+nuevo_usuario = Usuario(email="admin@systeplus.com", password=hashed_password)
 session_credenciales.add(nuevo_usuario)
 session_credenciales.commit()
 
